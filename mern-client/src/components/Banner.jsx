87ee355dd78f09b8a0202f1892723c8d3a6c68f0
Smridhi gupta <smridhi106@gmail.com>
2024-07-23 15:54:24 +0530

    init

--- mern-client/src/components/Banner.jsx
+++ mern-client/src/components/Banner.jsx
@@ -5,11 +5,12 @@ const Banner = () => {
     <div className='px-4 lg:px-24 bg-teal-100 flex items-center'>
         <div className='flex flex-col md:flex-row justify-between items-center gap-12 py-40'>
             {/*left side*/}
-            <div>
+            <div className='space-x-7'>
                 <h2 className='text-5xl font-bold leading-snug text-black'>Buy and Sell Your Books</h2>
-                <p></p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                 <div>
-                  <input type='search' name='search' id='search' placeholder='Search a Book' className=''/>
+                  <input type='search' name='search' id='search' placeholder='Search a Book' className='py-2 px-2 rounded-s-sm outline-none'/>
+                  <button className='bg-blue-700 px-6 py-2 text-white font-medium hover:bg-black transition-all ease-in duration-200'>Search</button>
                 </div>
             </div>
 
