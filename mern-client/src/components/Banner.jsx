01ea3db30c956de6e0b930136c195f5e767df48a
Smridhi gupta <smridhi106@gmail.com>
2024-07-24 12:37:50 +0530

    init

--- mern-client/src/components/Banner.jsx
+++ mern-client/src/components/Banner.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import BannerCard from '../home/BannerCard'
 
 const Banner = () => {
   return (
@@ -6,7 +7,7 @@ const Banner = () => {
         <div className='flex flex-col md:flex-row justify-between items-center gap-12 py-40'>
             {/*left side*/}
             <div className='md:w-1/2 space-y-8 h-full'>
-                <h2 className='text-6xl font-bold leading-snug text-black'>Buy and Sell Your Books <span></span></h2>
+                <h2 className='text-5xl font-bold leading-snug text-black'>Buy and Sell Your Books <span className='text-blue-700'>for the Best Prices</span></h2>
                 <p className='md:w-4/5'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                 <div>
                   <input type='search' name='search' id='search' placeholder='Search a Book' className='py-2 px-2 rounded-s-sm outline-none'/>
@@ -15,7 +16,9 @@ const Banner = () => {
             </div>
 
             {/*right side*/}
-            <div>Right Side</div>
+            <div>
+              <BannerCard/>
+            </div>
         </div>
     </div>
   )
