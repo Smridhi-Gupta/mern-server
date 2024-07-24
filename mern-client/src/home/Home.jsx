e9b3c04135d58edbabcca3a13bfcb91ad926ff36
Smridhi gupta <smridhi106@gmail.com>
2024-07-25 00:36:54 +0530

    init

--- mern-client/src/home/Home.jsx
+++ mern-client/src/home/Home.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import Banner from "../components/Banner";
 import FavouriteBooks from "./FavouriteBooks";
+import FavBook from "./FavBook";
+import PromoBanner from "./PromoBanner";
+import OtherBooks from "./OtherBooks";
 
 const Home = () => {
   return (
     <div>
-        <Banner/>
-        <FavouriteBooks/>
+      <Banner />
+      <FavouriteBooks />
+      <FavBook />
+      <PromoBanner />
+      <OtherBooks />
     </div>
   );
 };
--- mern-client/src/home/OtherBooks.jsx
+++ mern-client/src/home/OtherBooks.jsx
@@ -1,20 +1,21 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
+import { useState, useEffect } from "react";
 import BookCards from "../components/BookCards";
 
-const FavouriteBooks = () => {
+const OtherBooks = () => {
   const [books, setBooks] = useState([]);
 
   useEffect(() => {
     fetch("http://localhost:5000/all-books")
       .then((res) => res.json())
-      .then((data) => setBooks(data.slice(0, 8)));
+      .then((data) => setBooks(data.slice(4, 8)));
   }, []);
 
   return (
     <div>
-      <BookCards books={books} headline="Best Seller Books" />
+      <BookCards books={books} headline="Other Books" />
     </div>
   );
 };
 
-export default FavouriteBooks;
+export default OtherBooks;
