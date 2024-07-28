0d0bcc7128aca397c2fbaade8985f15c6ba14720
Smridhi gupta <smridhi106@gmail.com>
2024-07-28 18:18:39 +0530

    init

--- mern-client/src/dashboard/UploadBook.jsx
+++ mern-client/src/dashboard/UploadBook.jsx
@@ -1,30 +1,95 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Checkbox, Label, TextInput } from "flowbite-react";
 
 const UploadBook = () => {
+  const bookCategories = [
+    "Fiction",
+    "Non-Fiction",
+    "Mistery",
+    "Programming",
+    "Science Fiction",
+    "Fantasy",
+    "Horror",
+    "Bibliography",
+    "Autobiography",
+    "History",
+    "Self-help",
+    "Memoir",
+    "Business",
+    "Children Books",
+    "Travel",
+    "Religion",
+    "Art and Design",
+  ];
+
+  const [selectedBookCategory, setselectedBookCategory] = useState(
+    bookCategories[0]
+  );
+
+  const handleChangeSelectedValue = (event) => {
+    // console.log(event.target.value);
+    setselectedBookCategory(event.target.value);
+  };
   return (
     <div className="px-4 my-12">
       <h2 className="mb-8 text-3xl font-bold">Upload a Book</h2>
 
-      <form className="flex max-w-md flex-col gap-4">
-      <div>
-        <div className="mb-2 block">
-          <Label htmlFor="email1" value="Your email" />
+      <form className="flex lg:w-[1180px] flex-col flex-wrap gap-4">
+        {/* first row */}
+        <div className="flex gap-8">
+          {/* bookName */}
+          <div className="lg:w-1/2">
+            <div className="mb-2 block">
+              <Label htmlFor="bookTitle" value="Book Title" />
+            </div>
+            <TextInput
+              id="bookTitle"
+              name="bookTitle"
+              placeholder="Book name"
+              required
+              type="text"
+            />
+          </div>
+
+          {/* authorName */}
+          <div className="lg:w-1/2">
+            <div className="mb-2 block">
+              <Label htmlFor="authorName" value="Author Name" />
+            </div>
+            <TextInput
+              id="authorName"
+              name="authorName"
+              placeholder="Author Name"
+              required
+              type="text"
+            />
+          </div>
         </div>
-        <TextInput id="email1" type="email" placeholder="name@flowbite.com" required />
-      </div>
-      <div>
-        <div className="mb-2 block">
-          <Label htmlFor="password1" value="Your password" />
+
+        {/* second row */}
+        <div className="flex gap-8">
+          {/* imageURL */}
+          <div className="lg:w-1/2">
+            <div className="mb-2 block">
+              <Label htmlFor="imageURl" value="Book image URl" />
+            </div>
+            <TextInput
+              id="imageURl"
+              name="imageURl"
+              placeholder="Book image URl"
+              required
+              type="text"
+            />
+          </div>
+
+          {/* category */}
+          <div className="lg:w-1/2">
+            <div className="mb-2 block">
+              <Label htmlFor="inputState" value="Book Category" />
+            </div>
+          </div>
         </div>
-        <TextInput id="password1" type="password" required />
-      </div>
-      <div className="flex items-center gap-2">
-        <Checkbox id="remember" />
-        <Label htmlFor="remember">Remember me</Label>
-      </div>
-      <Button type="submit">Submit</Button>
-    </form>
+      </form>
     </div>
   );
 };
