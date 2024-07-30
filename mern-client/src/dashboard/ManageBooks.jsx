9f746d012e06745ad43528ce73d6856e9b44865a
Smridhi gupta <smridhi106@gmail.com>
2024-07-30 23:59:06 +0530

    init

--- mern-client/src/dashboard/ManageBooks.jsx
+++ mern-client/src/dashboard/ManageBooks.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const ManageBooks = () => {
   return (
-    <div>ManageBooks</div>
+    <div className='px-4 my-12'>
+      <h2 className='mb-8 text-3xl font-bold'>Manage Your Books</h2>
+    </div>
   )
 }
 
