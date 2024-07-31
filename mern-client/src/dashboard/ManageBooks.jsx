c80210ab847087039338105586244911a6350f34
Smridhi gupta <smridhi106@gmail.com>
2024-07-31 15:12:26 +0530

    init

--- mern-client/src/dashboard/ManageBooks.jsx
+++ mern-client/src/dashboard/ManageBooks.jsx
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Table } from "flowbite-react";
 
 const ManageBooks = () => {
+  const [allBooks, setAllBooks] = useState([]);
+
+  useEffect( () => {
+    fetch("http://localhost:5000/all-books").then(res => res.json()).then(data => )
+  }, [])
+
   return (
     <div className="px-4 my-12">
       <h2 className="mb-8 text-3xl font-bold">Manage Your Books</h2>
@@ -9,7 +15,7 @@ const ManageBooks = () => {
       {/* table for book data*/}
       <Table className="lg:w-[1180px]">
         <Table.Head>
-          <Table.HeadCell>Product name</Table.HeadCell>
+          <Table.HeadCell>No</Table.HeadCell>
           <Table.HeadCell>Product name</Table.HeadCell>
           <Table.HeadCell>Color</Table.HeadCell>
           <Table.HeadCell>Category</Table.HeadCell>
@@ -21,13 +27,19 @@ const ManageBooks = () => {
         <Table.Body className="divide-y">
           <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
             <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-              {'Apple MacBook Pro 17"'}
+              01
+            </Table.Cell>
+            <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+              "Apple MacBook Pro 17"
             </Table.Cell>
             <Table.Cell>Sliver</Table.Cell>
             <Table.Cell>Laptop</Table.Cell>
             <Table.Cell>$2999</Table.Cell>
             <Table.Cell>
-              <a href="#" className="font-medium text-cyan-600 hover:underline dark:text-cyan-500">
+              <a
+                href="#"
+                className="font-medium text-cyan-600 hover:underline dark:text-cyan-500"
+              >
                 Edit
               </a>
             </Table.Cell>
@@ -40,18 +52,26 @@ const ManageBooks = () => {
             <Table.Cell>Laptop PC</Table.Cell>
             <Table.Cell>$1999</Table.Cell>
             <Table.Cell>
-              <a href="#" className="font-medium text-cyan-600 hover:underline dark:text-cyan-500">
+              <a
+                href="#"
+                className="font-medium text-cyan-600 hover:underline dark:text-cyan-500"
+              >
                 Edit
               </a>
             </Table.Cell>
           </Table.Row>
           <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
-            <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">Magic Mouse 2</Table.Cell>
+            <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+              Magic Mouse 2
+            </Table.Cell>
             <Table.Cell>Black</Table.Cell>
             <Table.Cell>Accessories</Table.Cell>
             <Table.Cell>$99</Table.Cell>
             <Table.Cell>
-              <a href="#" className="font-medium text-cyan-600 hover:underline dark:text-cyan-500">
+              <a
+                href="#"
+                className="font-medium text-cyan-600 hover:underline dark:text-cyan-500"
+              >
                 Edit
               </a>
             </Table.Cell>
