54aa9c2e0717edd478d636ee04277a56833b81d6
Smridhi gupta <smridhi106@gmail.com>
2024-08-01 12:42:59 +0530

    init

--- mern-client/src/dashboard/ManageBooks.jsx
+++ mern-client/src/dashboard/ManageBooks.jsx
@@ -18,33 +18,33 @@ const ManageBooks = () => {
 
       {/* table for book data*/}
 
-      <table className="w-full border-collapse lg-w-[1180px]">
-        <thead>
+      <table className="w-full border-collapse lg-w-[1180px] text-left text-gray-500 dark:text-gray-400 shadow-xl rounded-md">
+        <thead className="text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
           <tr>
-            <th>NO.</th>
-            <th>BOOK NAME</th>
-            <th>AUTHOR NAME</th>
-            <th>CATEGORY</th>
-            <th>PRICES</th>
-            <th>EDIT OR MANAGE</th>
+            <th className="px-6 py-3">No.</th>
+            <th className="px-6 py-3">Book Name</th>
+            <th className="px-6 py-3">Author Name</th>
+            <th className="px-6 py-3">Category</th>
+            <th className="px-6 py-3">Prices</th>
+            <th className="px-6 py-3">Edit Or Manage</th>
           </tr>
         </thead>
         {allBooks.map((book, index) => (
           <tbody>
-            <tr key={book._id} className="border-b border-gray-300">
-              <td className="whitespece-nowrap font-medium text-gray-900 dark:text-white">{index + 1}</td>
-              <td className="whitespece-nowrap font-medium text-gray-900 dark:text-white">{book.bookTitle}</td>
-              <td>{book.authorName}</td>
-              <td>{book.category}</td>
-              <td>$10.00</td>
-              <td>
+            <tr key={book._id} className="bg-white border-b dark:bg-gray-800 drak:border-gray-700">
+              <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">{index + 1}</td>
+              <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">{book.bookTitle}</td>
+              <td className="px-6 py-4">{book.authorName}</td>
+              <td className="px-6 py-4">{book.category}</td>
+              <td className="px-6 py-4">$10.00</td>
+              <td className="px-6 py-4">
                 <Link
                   to={`/admin/dashboard/edit-books/${book._id}`}
                   className="font-medium text-cyan-600 hover:underline dark:text-cyan-500"
                 >
                   Edit
                 </Link>
-                <button className="">Delete</button>
+                <button className="bg-red-600">Delete</button>
               </td>
             </tr>
           </tbody>
