33be12044d7000b21a6b718a04699ca595062606
Smridhi gupta <smridhi106@gmail.com>
2024-08-04 20:40:42 +0530

    init

--- mern-client/src/dashboard/Sidebaar.jsx
+++ mern-client/src/dashboard/Sidebaar.jsx
@@ -26,7 +26,7 @@ const Sidebaar = () => {
           }
         </p>
       </Sidebar.Logo>
-      <Sidebar.Items className="items-left">
+      <Sidebar.Items className="bg-black">
         <Sidebar.ItemGroup>
 
           <Sidebar.Item href="/admin/dashboard" icon={HiChartPie}>
